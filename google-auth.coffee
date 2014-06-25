0805d7634f08f306263ddc88baf5b4de7da64c04
Henrique Pinheiro <git@gampoc.com>
2014-06-25 20:56:25 -0300

    Initial Commit

---
+++ google-auth.coffee
@@ -0,0 +1,95 @@
+do (angular, window) ->
+  'use strict'
+  angular
+  .module 'googleAuth', ['ng']
+  .provider 'gAuth', ->
+    client_id =
+    hd =
+    @setClientId = (clientId) ->
+      client_id = clientId
+    @setHD = (HD) ->
+      hd = HD
+
+    @$get = ['$q', ($q) ->
+      scopes = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+      
+      gapiLoadedPromise = do ->
+        defer = $q.defer()
+        #$.getScript 'https://apis.google.com/js/client.js', ->
+        $.getScript 'https://apis.google.com/js/client.js', ->
+          do _loop = ->
+            setTimeout ->
+              if not window.gapi or not window.gapi.auth
+                _loop()
+              else
+                setTimeout ->
+                  defer.resolve window.gapi
+                , 1
+            , 10
+        defer.promise
+
+      tokenInfo = false
+
+      checkSessionState = ->
+        defer = $q.defer()
+        if !client_id
+          defer.reject new Error "Please configure client_id with setClientId"
+          return defer.promise
+        gapiLoadedPromise.then (gapi) ->
+          gapi.auth.checkSessionState
+            client_id: client_id
+            hd: hd
+          , (disconnected) ->
+            defer.resolve !disconnected    
+        defer.promise
+
+      refreshLoginToken = (showPrompt = false) ->
+        defer = $q.defer()
+        if !client_id
+          defer.reject new Error "Please configure client_id with setClientId"
+          return defer.promise
+        checkSessionState().then (ok) ->
+          if not ok and not showPrompt
+            defer.resolve false
+          else
+            immediate = if showPrompt then false else true
+            gapi.auth.authorize
+              client_id: client_id
+              scope: scopes
+              immediate:immediate
+              hd: hd
+            , (json) ->
+              defer.resolve json       
+        defer.promise
+
+      #return this object
+      refreshLoginToken : refreshLoginToken
+      getUserInfoFromGoogle : (showPrompt = false) ->
+        tokenPromise = do ->
+          defer = $q.defer()
+          if tokenInfo and not tokenInfo.error?
+            defer.resolve tokenInfo
+            defer.promise
+          else
+            refreshLoginToken showPrompt
+        tokenPromise.then (_tokenInfo) ->
+          tokenInfo = _tokenInfo
+          defer = $q.defer()
+          if _tokenInfo and not _tokenInfo.error?
+            gapiLoadedPromise.then (gapi) ->
+              gapi.client.request path:'/oauth2/v2/userinfo'
+              .execute (user) ->
+                defer.resolve user
+          else
+            defer.resolve false
+          defer.promise
+      setScopes : (_scopes) ->
+        if Array.isArray _scopes
+          scopes = _scopes.join " "
+        else
+          scopes = _scopes
+      getScopes : ->
+        _scopes.split " "
+    ]
+    return
+  return
---
+++ google-auth.js
@@ -0,0 +1,129 @@
+(function(angular, window) {
+  'use strict';
+  angular.module('googleAuth', ['ng']).provider('gAuth', function() {
+    var client_id, hd;
+    client_id = hd = this.setClientId = function(clientId) {
+      return client_id = clientId;
+    };
+    this.setHD = function(HD) {
+      return hd = HD;
+    };
+    this.$get = [
+      '$q', function($q) {
+        var checkSessionState, gapiLoadedPromise, refreshLoginToken, scopes, tokenInfo;
+        scopes = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
+        gapiLoadedPromise = (function() {
+          var defer;
+          defer = $q.defer();
+          $.getScript('https://apis.google.com/js/client.js', function() {
+            var _loop;
+            return (_loop = function() {
+              return setTimeout(function() {
+                if (!window.gapi || !window.gapi.auth) {
+                  return _loop();
+                } else {
+                  return setTimeout(function() {
+                    return defer.resolve(window.gapi);
+                  }, 1);
+                }
+              }, 10);
+            })();
+          });
+          return defer.promise;
+        })();
+        tokenInfo = false;
+        checkSessionState = function() {
+          var defer;
+          defer = $q.defer();
+          if (!client_id) {
+            defer.reject(new Error("Please configure client_id with setClientId"));
+            return defer.promise;
+          }
+          gapiLoadedPromise.then(function(gapi) {
+            return gapi.auth.checkSessionState({
+              client_id: client_id,
+              hd: hd
+            }, function(disconnected) {
+              return defer.resolve(!disconnected);
+            });
+          });
+          return defer.promise;
+        };
+        refreshLoginToken = function(showPrompt) {
+          var defer;
+          if (showPrompt == null) {
+            showPrompt = false;
+          }
+          defer = $q.defer();
+          if (!client_id) {
+            defer.reject(new Error("Please configure client_id with setClientId"));
+            return defer.promise;
+          }
+          checkSessionState().then(function(ok) {
+            var immediate;
+            if (!ok && !showPrompt) {
+              return defer.resolve(false);
+            } else {
+              immediate = showPrompt ? false : true;
+              return gapi.auth.authorize({
+                client_id: client_id,
+                scope: scopes,
+                immediate: immediate,
+                hd: hd
+              }, function(json) {
+                return defer.resolve(json);
+              });
+            }
+          });
+          return defer.promise;
+        };
+        return {
+          refreshLoginToken: refreshLoginToken,
+          getUserInfoFromGoogle: function(showPrompt) {
+            var tokenPromise;
+            if (showPrompt == null) {
+              showPrompt = false;
+            }
+            tokenPromise = (function() {
+              var defer;
+              defer = $q.defer();
+              if (tokenInfo && (tokenInfo.error == null)) {
+                defer.resolve(tokenInfo);
+                return defer.promise;
+              } else {
+                return refreshLoginToken(showPrompt);
+              }
+            })();
+            return tokenPromise.then(function(_tokenInfo) {
+              var defer;
+              tokenInfo = _tokenInfo;
+              defer = $q.defer();
+              if (_tokenInfo && (_tokenInfo.error == null)) {
+                gapiLoadedPromise.then(function(gapi) {
+                  return gapi.client.request({
+                    path: '/oauth2/v2/userinfo'
+                  }).execute(function(user) {
+                    return defer.resolve(user);
+                  });
+                });
+              } else {
+                defer.resolve(false);
+              }
+              return defer.promise;
+            });
+          },
+          setScopes: function(_scopes) {
+            if (Array.isArray(_scopes)) {
+              return scopes = _scopes.join(" ");
+            } else {
+              return scopes = _scopes;
+            }
+          },
+          getScopes: function() {
+            return _scopes.split(" ");
+          }
+        };
+      }
+    ];
+  });
+})(angular, window);
